836e43079c67a41623056336a4d97d7b9d12323a
Ian <ianmchristian@gmail.com>
2021-11-01 15:33:01 -0400

    home page responsive and link fixes for dropdown menu

--- src/app/Components/nav-bar/nav-bar.component.ts
+++ src/app/Components/nav-bar/nav-bar.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  isChecked!: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.isChecked);
   }
 
+  // Close the hamburger menu on link click
+
+  async closeMenu() {
+    this.isChecked = true;
+    await this.animationTimer(1) // menu will not close every time without this delay -.-
+    this.isChecked = false;
+    
+    
+  }
+
+  async animationTimer (ms: number) {
+    return new Promise(res => setTimeout(res, ms));
+  }
 }
